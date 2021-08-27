1094cdc4738a77cfaccbf1bb15d1b8d963feece5
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-27 18:33:41 +0300

    add filter

--- react-fundamental-course-main/src/App.jsx
+++ react-fundamental-course-main/src/App.jsx
@@ -1,25 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import PostList from "components/PostList";
 
 import PostForm from "components/PostForm";
 
+import PostFilter from "components/PostFilter";
+
 export default function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript 1", body: "Description" },
-    { id: 2, title: "Javascript 2", body: "Description" },
-    { id: 3, title: "Javascript 3", body: "Description" },
+    { id: 1, title: "Javascript 1", body: "1Description" },
+    { id: 2, title: "Javascript 0", body: "2Description" },
+    { id: 3, title: "Javascript 3", body: "0Description" },
   ]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
 
+  const sortedPosts = useMemo(() => {
+    console.log("getSortedPosts");
+    if (filter.sort) {
+      return [...posts].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
+    }
+    return posts;
+  }, [filter.sort, posts]);
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLocaleLowerCase().includes(filter.query)
+    );
+  }, [filter.query, sortedPosts]);
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} heading="List posts JS" />
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+
+      {sortedAndSearchedPosts.length !== 0 ? (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          heading="List posts JS"
+        />
+      ) : (
+        <h2 style={{ textAlign: "center" }}>No posts found!</h2>
+      )}
     </div>
   );
 }
---
+++ react-fundamental-course-main/src/components/PostFilter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import MyInput from "components/UI/input/MyInput";
+import MySelect from "components/UI/select/MySelect";
+
+const PostFilter = ({ filter, setFilter }) => {
+  return (
+    <div>
+      <MyInput
+        value={filter.query}
+        onChange={(e) => setFilter({ ...filter, query: e.target.value })}
+        placeholder="Поиск..."
+      />
+      <MySelect
+        value={filter.sort}
+        onChange={(selectedSort) =>
+          setFilter({ ...filter, sort: selectedSort })
+        }
+        defaultValue="filter"
+        options={[
+          { value: "title", name: "Name" },
+          { value: "body", name: "Description" },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default PostFilter;
